c2f91cb14b2d7404343f0ceefac1dcb53f546c67
Overdese <overdese@gmail.com>
2015-10-14 15:14:10 +0300

    add disqus

--- macros.py
+++ macros.py
@@ -83,7 +83,21 @@ def hook_preconvert_labels():
 # comments
 
 def disqus_comments():
-    return '''КОММЕНТЫ!'''
+    return '''
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES * * */
+        var disqus_shortname = 'overdese-com';
+        
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+    '''
 
 # preview posts
 
